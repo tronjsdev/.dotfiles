90559d0078b1bb7305144392635776d654f2a626
tronjs <tronjs.dev@gmail.com>
2019-11-05 08:53:50 +0700

    update herbstluftwm panel

--- bin/bin/autorun.sh
+++ bin/bin/autorun.sh
@@ -7,16 +7,16 @@ killall ibus-daemon
 killall nm-applet
 killall pamac-tray
 killall polkit-mate-aut
-killall stalonetray
+#killall stalonetray
 
-#alttab -fg "#9EB7C0" -bg "#222D31" -frame "#1E90FF" -t 128x150 -i 127x64 &
+alttab -fg "#9EB7C0" -bg "#222D31" -frame "#0049FF" -t 128x150 -i 127x64 &
 xset b off
-nitrogen --restore; sleep 1
-compton -bC &
-dunst &
-#feh --bg-scale ~/wallpaper.jpg &
 ibus-daemon -drx &
 nvidia-settings -a [gpu:0]/GPUPowerMizerMode=1 &
+nitrogen --restore; sleep 1
+#feh --bg-scale ~/wallpaper.jpg &
+compton -bC &
+dunst &
 xinput set-prop 9 'libinput Accel Speed' 0.5 &
 /usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
 nm-applet &
--- herbstluftwm/.config/herbstluftwm/panel.sh
+++ herbstluftwm/.config/herbstluftwm/panel.sh
@@ -198,6 +198,7 @@ hc pad $monitor $panel_height
     -e "button3=;button4=exec:$hc_quoted use_index -1;button5=exec:$hc_quoted use_index +1" \
     -ta l -bg "$bgcolor" -fg '#efefef'
 
-
-sleep 6
-stalonetray -c ~/.config/stalonetray/config
+sleep 7
+killall stalonetray
+sleep 1
+stalonetray -c ~/.config/stalonetray/config &
