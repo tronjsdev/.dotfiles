a3ee87c648cd90d6a922f7567caaa615c8a63d99
tronjsdev <tronjs.dev@gmail.com>
2020-01-29 00:02:44 +0700

    update

---
+++ bin/bin/user-overrides.js
@@ -0,0 +1,67 @@
+//user_pref("media.peerconnection.enabled", true);
+
+//user_pref("security.mixed_content.block_display_content", false);
+//user_pref("security.mixed_content.block_object_subrequest", false);
+user_pref("network.http.referer.XOriginPolicy", 0);
+
+// user_pref("media.gmp-widevinecdm.visible", true);
+// user_pref("media.gmp-widevinecdm.enabled", true);
+// user_pref("media.eme.enabled", true);
+
+user_pref("dom.serviceWorkers.enabled", true);
+user_pref("privacy.firstparty.isolate", false);
+
+user_pref("browser.startup.page", 3);
+user_pref("browser.display.use_document_fonts", 1);
+user_pref("gfx.font_rendering.opentype_svg.enabled", true);
+user_pref("gfx.font_rendering.graphite.enabled", true);
+
+user_pref("browser.newtabpage.enabled", true);
+user_pref("browser.newtab.preload", true);
+
+
+
+
+user_pref("privacy.sanitize.sanitizeOnShutdown", true);
+user_pref("privacy.clearOnShutdown.cache", false);
+user_pref("privacy.clearOnShutdown.cookies", false);
+user_pref("privacy.clearOnShutdown.downloads", false); // see note above
+user_pref("privacy.clearOnShutdown.formdata", true); // Form & Search History
+user_pref("privacy.clearOnShutdown.history", false); // Browsing & Download History
+user_pref("privacy.clearOnShutdown.offlineApps", true); // Offline Website Data
+user_pref("privacy.clearOnShutdown.sessions", false); // Active Logins
+user_pref("privacy.clearOnShutdown.siteSettings", false); // Site Preferences
+
+user_pref("privacy.cpd.cache", false);
+user_pref("privacy.cpd.cookies", false);
+// user_pref("privacy.cpd.downloads", true); // not used, see note above
+user_pref("privacy.cpd.formdata", true); // Form & Search History
+user_pref("privacy.cpd.history", false); // Browsing & Download History
+user_pref("privacy.cpd.offlineApps", true); // Offline Website Data
+user_pref("privacy.cpd.passwords", false); // this is not listed
+user_pref("privacy.cpd.sessions", false); // Active Logins
+user_pref("privacy.cpd.siteSettings", false); // Site Preferences
+
+
+user_pref("privacy.resistFingerprinting", false);
+user_pref("privacy.resistFingerprinting.letterboxing", false); // [HIDDEN PREF]
+
+user_pref("security.secure_connection_icon_color_gray", false);
+
+//user_pref("devtools.toolbox.zoomValue", "1.2");
+
+
+user_pref("font.size.monospace.x-western", 16);
+user_pref("font.size.monospace.x-unicode", 16);
+
+user_pref("font.name.serif.x-unicode", "monospace");
+user_pref("font.name.serif.x-western", "monospace");
+
+user_pref("browser.download.useDownloadDir", true);
+
+
+
+//user_pref("network.cookie.cookieBehavior", 0);
+//user_pref("network.cookie.thirdparty.sessionOnly", false);
+//user_pref("network.cookie.thirdparty.nonsecureSessionOnly", false); // [FF58+]
+//user_pref("privacy.donottrackheader.enabled", false);
